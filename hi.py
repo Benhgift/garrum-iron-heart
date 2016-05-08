f5e0d1838a0c2fb85b890d62539b7b52558f8ad0
Ben Gift <benhgift@gmail.com>
2016-05-08 01:06:26 -0700

    working server/client

--- hi.py
+++ hi.py
@@ -1,2 +1,38 @@
 import socket
-print(dir(socket))
+import sys
+
+
+def server():
+    print("server")
+    server = socket.socket()
+    host = socket.gethostname()
+    port = 4951
+    s.bind((host, port))
+    
+    server.listen(5)
+    while True:
+        client, address = server.accept()
+        print(address)
+        client.send("Ground control to Major Tom")
+        client.close()
+   
+    
+def client():
+    print("client")
+    server = socket.socket()
+    host = '98.146.149.203' #socket.gethostname()
+    port = 4951
+    
+    server.connect((host, port))
+    print(server.recv(1024))
+    server.close()
+    
+
+if __name__ == '__main__':
+    try: 
+        if sys.argv[1] == 'server':
+            server()
+        else:
+            client()
+    except:
+        pass
